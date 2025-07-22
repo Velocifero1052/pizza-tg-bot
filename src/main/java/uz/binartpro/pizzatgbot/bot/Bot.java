9d179f85dc61d962197490db3dd871965acd16b4
Velocifero1052 <commelevent1052@yandex.ru>
2025-07-23 00:34:41 +0500

    deleted creds

--- src/main/java/uz/binartpro/pizzatgbot/bot/Bot.java
+++ src/main/java/uz/binartpro/pizzatgbot/bot/Bot.java
@@ -1,6 +1,6 @@
 package uz.binartpro.pizzatgbot.bot;
 
 public class Bot {
-    static final String BOT_TOKEN = "7657937605:AAEhYRZ6YxgAB97tsreNnuXe9xQtkX9b0gE";
-    static final String BOT_USERNAME = "dunnoHowToFindName_bot";
+    static final String BOT_TOKEN = "";
+    static final String BOT_USERNAME = "";
 }
